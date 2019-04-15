9e57ac2ffbeff7713407d270c587f04c9286b6fa
Ghoppen <38247650+Ghoppen@users.noreply.github.com>
2019-04-15 14:49:23 +0200

    Collider update

--- src/GameEngine/Collider.java
+++ src/GameEngine/Collider.java
@@ -22,16 +22,18 @@ public class Collider {
         this.attacker = attacker;
         this.entitySet = entitySet;
     }
+    public Collider(EntitySet enititySet){
+        this.entitySet = enititySet;
+    }
 
-    public List checkSuroundings() {
+    public List checkSuroundings(Entity entity) {
         List<Entity> surroundingEntities = new ArrayList<Entity>();
         for (int i = -6; i < 7; i++) {
             for (int z = -6; z < 7; z++) {
-                XY dummySearch = this.attacker.getPosition();
-                if (new XY(dummySearch.getX() + i, dummySearch.getY() + z) == null) {
+                if (new XY(entity.getPosition().getX() + i, entity.getPosition().getY() + z) == null) {
                     continue;
                 } else {
-                    surroundingEntities.add(entitySet.findEntity(new XY(dummySearch.getX() + i, dummySearch.getY() + z)));
+                    surroundingEntities.add(entitySet.findEntity(new XY(entity.getPosition().getX() + i, entity.getPosition().getY() + z)));
                 }
             }
 
@@ -39,7 +41,8 @@ public class Collider {
         return surroundingEntities;
     }
 
-    public void resolveColision(){
+    public void resolveColision(Entity current){
+
 
     }
 }
