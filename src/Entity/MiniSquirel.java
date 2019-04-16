1a98ecec6547d408afde07fd119a0302d5080507
LukasMarq <38247512+LukasMarq@users.noreply.github.com>
2019-04-16 13:34:57 +0200

    Default Changelist

--- src/Entity/MiniSquirel.java
+++ src/Entity/MiniSquirel.java
@@ -8,16 +8,38 @@ import java.util.List;
 public class MiniSquirel extends MasterSquirel {
 
 
-    public MiniSquirel(int energy, XY pos){
-        super(energy,pos);
+    public MiniSquirel(int energy, XY pos) {
+        super(energy, pos);
     }
 
     public List<MiniSquirel> getSuperMiniSquirelList() {
-         return super.getMiniSquirelList();
+        return super.getMiniSquirelList();
     }
 
     @Override
     public void nextStep(EntityContext context) {
-        this.setPosition(this.getPosition().getNewPosition());
+
+        List surround = context.checkSuroundings(this);
+        XY direction = null;
+        for (Object entity : surround) {
+
+
+            if (entity instanceof GoodBeast || entity instanceof GoodPlant) {
+                int x = entity.getPosition().getX() - this.getPosition().getX();
+                int y = entity.getPosition().getY() - this.getPosition().getY();
+                direction = this.culcRun(x, y);
+
+            }
+            if (entity instanceof BadBeast || entity instanceof BadPlant) {
+                int x = this.getPosition().getX() - entity.getPosition().getX();
+                int y = this.getPosition().getY() - entity.getPosition().getY();
+                direction = this.culcRun(x, y);
+            }
+
+        }
+        if (direction != null)
+            context.tryMove(this, direction);
+
+
     }
 }
